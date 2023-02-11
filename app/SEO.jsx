f02e4c952bcbaa8f0eaca083bc767737cc086e93
Oliver Saxon <oliversaxon@olivers-air.home>
2023-02-11 12:07:28 +0000

    feat: added favicons

--- app/SEO.jsx
+++ app/SEO.jsx
@@ -16,8 +16,53 @@ const SEO = ({ title, postPage = false }) => {
                 rel="canonical"
                 href={!slug ? siteConfig.url : `${siteConfig.url}/${slug}`}
             />
+            {/* Open Graph */}
+            <meta property="og:type" content={siteConfig.type} />
+            <meta property="og:site_name" content={siteConfig.name} />
+            <meta property="og:description" content={siteConfig.description} />
+            <meta property="og:title" content={siteConfig.title} />
+            <meta name="image" property="og:image" content={siteConfig.image} />
+            {/* Favicons */}
+            {favicons.map((linkProps) => (
+                <link key={linkProps.href} {...linkProps} />
+            ))}
         </>
     );
 };
+
+const favicons = [
+    {
+        rel: "apple-touch-icon",
+        sizes: "70x70",
+        href: "/favicon/apple-icon-70x70.png",
+    },
+    {
+        rel: "apple-touch-icon",
+        sizes: "144x144",
+        href: "/favicon/apple-icon-144x144.png",
+    },
+    {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        href: "/favicon/android-icon-192x192.png",
+    },
+    {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon/favicon-32x32.png",
+    },
+    {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon/favicon-16x16.png",
+    },
+    {
+        rel: "manifest",
+        href: "/favicon/site.webmanifest.json",
+    },
+];
 
 export default SEO;
