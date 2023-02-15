4fe1301534d539aa52f5d4e07c0516d4281d4bc1
Oliver Saxon <oliversaxon@olivers-air.home>
2023-02-15 18:20:55 +0000

    testing api routes

--- pages/api/notion/get-latest-posts.js
+++ pages/api/notion/get-latest-posts.js
@@ -11,31 +11,31 @@ export default async function handler(req, res) {
     if (req.method !== "GET") {
         return res.status(405).json(`${req.method} not allowed.`);
     }
-
+    console.log(req.query);
     const { api_key } = req.query;
 
-    if (api_key !== process.env.ZAPIER_API_KEY) {
+    if (api_key !== process.env.MY_API_KEY) {
+        console.log("unauthorised attempt");
         return res.status(400).json({ message: "Not authorised." });
-    }
-
-    try {
-        let { posts } = await getWeeklyRoundup({ limit: 5 });
-        posts = posts.map((post) => {
-            return {
-                title: getPostTitle(post),
-                excerpt: getPostExcerpt(post),
-                coverImage: getPostCoverImage(post),
-                slug: getPostSlug(post),
-                created: format(new Date(post.created_time), "dd-MMM-yyyy"),
-            };
-        });
-        if (posts) {
+    } else {
+        console.log("authorised");
+        try {
+            let { posts } = await getWeeklyRoundup({ limit: 5 });
+            posts = posts.map((post) => {
+                return {
+                    title: getPostTitle(post),
+                    excerpt: getPostExcerpt(post),
+                    coverImage: getPostCoverImage(post),
+                    slug: getPostSlug(post),
+                    created: format(new Date(post.created_time), "dd-MMM-yyyy"),
+                };
+            });
             return res.status(200).json(posts);
+        } catch (error) {
+            // err
+            res.status(500).json({
+                message: "There was an error retrieving posts",
+            });
         }
-    } catch (error) {
-        // err
-        res.status(500).json({
-            message: "There was an error retrieving posts",
-        });
     }
 }
