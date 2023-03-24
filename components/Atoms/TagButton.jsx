50b129dce98c699c80a680aed34a00bb47623dc9
Oliver Saxon <oliversaxon@Olivers-MacBook-Air.local>
2023-03-24 15:27:23 +0700

    improvements to notion block renderer

--- components/Atoms/TagButton.jsx
+++ components/Atoms/TagButton.jsx
@@ -4,23 +4,23 @@ import clsx from "clsx";
 
 import React, { useRef } from "react";
 
-const buttonConfig = {
-    blue: "bg-blue-400 text-blue-50 border",
-    orange: "bg-orange-400 text-orange-50",
-    green: "bg-green-400 text-green-50",
-    red: "bg-red-400 text-red-50",
-    purple: "bg-purple-400 text-purple-50",
-    yellow: "bg-yellow-400 text-yellow-50",
-    gray: "bg-gray-400 text-gray-50",
-};
+const buttonConfig = new Map();
+buttonConfig.set("blue", "bg-info text-blue-50 border");
+buttonConfig.set("orange", "bg-orange-400 text-orange-50");
+buttonConfig.set("green", "bg-success text-green-50");
+buttonConfig.set("red", "bg-error text-red-50");
+buttonConfig.set("purple", "bg-purple-400 text-purple-50");
+buttonConfig.set("yellow", "bg-yellow-400 text-yellow-50");
+buttonConfig.set("gray", "bg-primary text-gray-50");
+buttonConfig.set("default", "bg-primary text-primary-content");
 
 const TagButton = ({ tag }) => {
     return (
         <Link
             className={clsx(
-                `rounded-md px-2 py-1 bg-opacity-60 shadow ${
-                    buttonConfig[tag.color]
-                }`
+                `rounded-md bg-opacity-60 px-2 py-1 shadow ${buttonConfig.get(
+                    tag.color
+                )}`
             )}
             href="/"
         >
--- components/Organisms/NotionBlock.jsx
+++ components/Organisms/NotionBlock.jsx
@@ -1,10 +1,38 @@
-"use client";
 import React from "react";
 import Emoji from "../Atoms/Emoji";
 import Image from "next/image";
 import Link from "next/link";
 import Heading from "../Atoms/Heading";
 import clsx from "clsx";
+
+const convertImage = (w, h) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
+
+const toBase64 = (str) =>
+    typeof window === "undefined"
+        ? Buffer.from(str).toString("base64")
+        : window.btoa(str);
+
+const NOTION_BG_COLOUR = {
+    blue_background: "bg-info bg-opacity text-blue-50",
+    orange_background: "bg-orange-400 text-orange-50",
+    green_background: "bg-success text-green-50",
+    red_background: "bg-warning text-red-50",
+    purple_background: "bg-purple-400 text-purple-50",
+    yellow_background: "bg-yellow-400 text-yellow-50",
+    gray_background: "bg-gray-400 text-gray-50",
+};
 
 const BLOCK_TYPES = {
     h1: "heading_1",
@@ -57,7 +85,7 @@ const RichText = ({ rich_text, as: Element }) => {
     );
 };
 
-const NotionBlock = ({ block }) => {
+const NotionBlock = async ({ block }) => {
     switch (block.type) {
         case BLOCK_TYPES.h1:
             // For a heading
@@ -68,10 +96,7 @@ const NotionBlock = ({ block }) => {
             );
         case BLOCK_TYPES.h2:
             return (
-                <Heading
-                    as="h2"
-                    className="py-4 text-center text-3xl font-bold"
-                >
+                <Heading as="h2" className="py-4 text-3xl font-bold">
                     {block[BLOCK_TYPES.h2].rich_text[0].plain_text}
                 </Heading>
             );
@@ -97,7 +122,7 @@ const NotionBlock = ({ block }) => {
                     : null;
             const imgCaption = block[BLOCK_TYPES.image].caption;
             return (
-                <figure className="relative flex h-auto w-full flex-col items-center justify-center py-8">
+                <figure className="relative mx-auto flex h-auto max-w-[600px] flex-col py-8">
                     <Image
                         src={imgUrl}
                         width={600}
@@ -105,9 +130,10 @@ const NotionBlock = ({ block }) => {
                         alt={imgCaption ? imgCaption : "Image has no caption"}
                         priority
                         className="object-cover"
+                        placeholder="empty"
                     />
                     {imgCaption ? (
-                        <span className="w-full italic">
+                        <span className="max-w-[600px] italic">
                             {imgCaption[0]?.text.content}
                         </span>
                     ) : null}
@@ -153,9 +179,15 @@ const NotionBlock = ({ block }) => {
             const richText = block[BLOCK_TYPES.callout].rich_text;
             const itemCount = richText.length;
             const emoji = block[BLOCK_TYPES.callout].icon.emoji || null;
+
             return (
-                <span className="inline-flex gap-2 bg-primary p-4 text-primary-content">
-                    {emoji && <Emoji symbol={emoji} />}
+                <span
+                    className={clsx(
+                        "mt-8 mb-4 flex items-center gap-2 p-2 text-xl",
+                        NOTION_BG_COLOUR[block[BLOCK_TYPES.callout].color]
+                    )}
+                >
+                    {emoji && <Emoji className="text-xl" symbol={emoji} />}
                     {richText.map((item, index) => {
                         if (item.href !== null) {
                             return (
