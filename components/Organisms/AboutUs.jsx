2b2770f7399075f5ad2646ad0d1748694eca61bb
Oliver Saxon <oliversaxon@Olivers-MacBook-Air.local>
2023-03-26 20:59:21 +0700

    styled about us section

--- components/Organisms/AboutUs.jsx
+++ components/Organisms/AboutUs.jsx
@@ -2,6 +2,7 @@
 import SectionHeader from "../Atoms/SectionHeader";
 import { AnimatePresence, motion, useCycle } from "framer-motion";
 import Image from "next/image";
+import Emoji from "../Atoms/Emoji";
 
 const AboutUs = () => {
     const [open, cycleOpen] = useCycle(false, true);
@@ -9,18 +10,14 @@ const AboutUs = () => {
     return (
         <section className="layout flex flex-col items-start">
             <SectionHeader text="About Us" />
-            <p className="py-6">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Quisquam fugit vero rem pariatur ipsam magnam maxime sequi.
-                Laborum, iste aut!
-            </p>
+            <p className="py-6">Hi there! Our names are Oli & Giovanna.</p>
             <AnimatePresence>
                 {open && (
                     <motion.div
-                        className="bg-base-100"
+                        className="my-4 bg-base-100"
                         initial={{ height: 0, opacity: 0 }}
                         animate={{
-                            height: "calc(35vh)",
+                            height: "auto",
                             opacity: "100%",
                         }}
                         exit={{
@@ -33,33 +30,44 @@ const AboutUs = () => {
                         }}
                     >
                         <motion.div
-                            className="flex gap-6 p-3"
+                            className="flex flex-col items-center gap-6 p-3 md:flex-row md:items-stretch"
                             initial="closed"
                             animate="open"
                             exit="closed"
                         >
                             <Image
                                 src="https://res.cloudinary.com/djaiep6vj/image/upload/ar_1:1,c_fill,e_art:hokusai,g_auto,w_1000/v1678878475/travel_photos/DSC00156_DxO_iwq6df.jpg"
-                                width={248}
-                                height={165}
+                                width={200}
+                                height={200}
                                 alt="avatar of oli and giovanna"
-                                className="mask mask-circle"
+                                className="mask mask-hexagon"
                             />
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Sunt inventore fugiat, ratione
-                                perspiciatis necessitatibus deserunt vitae est
-                                porro ut possimus alias sint nobis hic cum unde,
-                                obcaecati quia animi excepturi, fuga voluptatem.
-                                Reprehenderit totam veniam minus animi dolores
-                                eligendi placeat maxime ullam accusantium
-                                temporibus nostrum labore unde illo impedit rem
-                                necessitatibus, eos laboriosam. Officia
-                                distinctio veritatis, dolor magnam aliquid error
-                                nam eos officiis similique beatae suscipit
-                                consectetur deleniti eius nulla vel odit!
-                                Dolorum, beatae at.
-                            </p>
+                            <section>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Sunt inventore fugiat,
+                                    ratione perspiciatis necessitatibus deserunt
+                                    vitae est porro ut possimus alias sint nobis
+                                    hic cum unde, obcaecati quia animi
+                                    excepturi, fuga voluptatem. Reprehenderit
+                                    totam veniam minus animi dolores eligendi
+                                    placeat maxime ullam accusantium
+                                </p>
+                                <div className="my-4 flex gap-4">
+                                    <a
+                                        className="link hover:link-success"
+                                        href="mailto:oliverrsaxon@gmail.com"
+                                    >
+                                        <Emoji symbol={"✉️"} /> Email us
+                                    </a>
+                                    <a
+                                        className="link hover:link-success"
+                                        href="https://www.workaway.info/en/workawayer/osaxon/journey"
+                                    >
+                                        <Emoji symbol={"🌏"} /> Workaway profile
+                                    </a>
+                                </div>
+                            </section>
                         </motion.div>
                     </motion.div>
                 )}
